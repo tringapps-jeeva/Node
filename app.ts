3b56ac5b62c5026fdb6bfbe243c8f3fe7ae434a7
tringapps-jeeva <jeeva.j@tringapps.com>
2023-03-06 10:23:41 +0530

    change1

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import router from "./users/user.router";
 import express from "express";
 const app = express();
 app.use(express.json());
-app.use("/user",router);
+app.use("/users",router);
 
 app.listen(6000, () => {
     console.log("server up and running");
